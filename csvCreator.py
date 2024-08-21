72544d9cf8f725f91e603c98b98ff3ae714b88b5
chienpc02 <phamchien01042002@gmail.com>
2024-08-21 15:53:47 +0700

    add mavsdk_server

--- csvCreator.py
+++ csvCreator.py
@@ -41,7 +41,7 @@ from functions.trajectories import *
 from functions.create_active_csv import create_active_csv
 
 # Example usage
-shape_name="circle"
+shape_name="helix"
 diameter = 30.0
 direction = 1
 maneuver_time = 90.0
